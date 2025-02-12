0475a565d0af93ebeed8ba35f99ac91caf7d6293
ch mahesh <chmahesh997@gmail.com>
2025-02-12 23:05:55 +0530

    feat: added backend routes

---
+++ backend/src/routes/userRouter.ts
@@ -0,0 +1,53 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
+import { Hono } from "hono";
+
+export const userRouter = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
+
+userRouter.post("/signup", async (c) => {
+  const reqData = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username: reqData.username,
+        password: reqData.password,
+        name: reqData.name,
+      },
+    });
+    const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+    return c.json({ token });
+  } catch (e) {
+    return c.text("already exists or invalid");
+  }
+});
+userRouter.post("/signin", async (c) => {
+  const reqData = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const user = await prisma.user.findFirst({
+    where: {
+      username: reqData.username,
+      password: reqData.password,
+    },
+  });
+
+  if (!user) {
+    c.status(401);
+    return c.json({ message: "Invalid username or password" });
+  }
+
+  const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+  c.status(200);
+  return c.json({ token });
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      Hey i am working on this .
+      <br />
+      <br />
+      <a href="https://github.com/mahe-gi/blog">
+        {" "}
+        Click Here To Checkout Codebase
+      </a>
+    </>
+  );
+}
+
+export default App;
