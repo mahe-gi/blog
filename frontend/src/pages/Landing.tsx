f61d441e6667a01eb059ede3e645ad950cf43fc5
ch mahesh <chmahesh997@gmail.com>
2025-02-18 00:27:40 +0530

    saf

---
+++ frontend/src/pages/Landing.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+
+function Landing() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/signup");
+  });
+  return (
+    <div className=" flex items-center justify-center h-screen w-screen">
+      <div>
+        <div>Redirecting ...</div>
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
